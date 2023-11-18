8d0dcf16944a1a04d7b6849d055e8b8c606d99ad
v7ja <142932805+v7ja@users.noreply.github.com>
2023-11-18 23:25:20 +0300

    Update rode.py

--- rode.py
+++ rode.py
@@ -7,7 +7,7 @@ qq = 0
 req = requests.get(f"https://t.me/{o}").text
 if "tgme_username_link" not in req:
 	print("No")
-	v = requests.post(f'https://api.telegram.org/bot{tok}/sendMessage?chat_id={idown}&text=- The user is used😡')
+	v = requests.post(f'https://api.telegram.org/bot{tok}/sendMessage?chat_id={idown}&text=- The user is used 📎')
 	exit("The user is used")
 while True:
 	for session in open("account.txt","r").read().split("\n"):
@@ -17,20 +17,21 @@ while True:
 					app = Client("ACC",api_id=24367955,api_hash="df9e7f5217331d03353a8d42e11419fc",session_string=session)
 					app.connect()
 					try:
-						ch = app.create_channel(title="𝐓𝐇𝐄 𝐁𝐄𝐒𝐓 ☠️")
+						ch = app.create_channel(title="𝖽𝗈𝗇𝖾 𝖻𝗒 , 𝖺𝖻𝗈𝗈𝖽")
 						ch = ch.id
 						app.set_chat_username(ch, o)
-						app.update_profile(first_name="- 𝐓𝐇𝐄 𝐁𝐄𝐒𝐓 ☠️<\>", bio="ᴄʜ ↬ @F55F5 | ᴅᴇᴠ ↬ @XBBBBBBBBB ↬ @O_C_t  ☠️ ")
+						app.update_profile(first_name="- ძ᥆ᥒ𝖾 α𝖻᥆᥆ძ | #1<\>", bio="𝖼𝗁 , @ToGoLang | 𝖽𝖾𝗏 , @kx_kkk")
 						qq+=1
-						op = requests.post(f'''https://api.telegram.org/bot{tok}/sendvideo?chat_id={idown}&video=https://telegra.ph/file/3a3fc7aa1b03a4a48c11e.mp4&caption=> Sorry Bot I'm Top 1
+						op = requests.post(f'''https://api.telegram.org/bot{tok}/sendvideo?chat_id={idown}&video=https://telegra.ph/file/bff1264778391cdd1635a.mp4&caption=>
 new   FLOOD
-  UserName: @{o}
-  Clicks: {qq}
-  Type:  Channel
-  BY : @RzRzR ↬ @O_C_t 🐊''')
+is a new Flood By : aBooD 🐊,
+এ〔 𝗎𝗌𝖾𝗋𝗇𝖺𝗆𝖾 〕: @{o}
+এ〔 𝖼𝗅𝗂𝖼𝗄𝗌 〕: {qq}
+এ〔 𝖼𝗁 〕: @ToGoLang
+এ〔 𝗍𝗒𝗉𝖾 〕: 𝖼𝗁𝖺𝗇𝗇𝖾𝗅''')
 						v = requests.post(f'https://api.telegram.org/bot{tok}/sendMessage?chat_id={idown}&text=[ {session} ]')
-						app.send_message(ch,f'''> Sorry but I'm the best  @XBBBBBBBBB ↬ @O_C_t ☠️''')
-						pl = requests.post(f'''https://api.telegram.org/bot5766161754:AAF_z9fYlBVlmj84YxIoLdP709sHTH0TFS8/sendvideo?chat_id=94784270&video=https://telegra.ph/file/3a3fc7aa1b03a4a48c11e.mp4&caption=> Sorry but I'm the best ↬\nnew   FLOOD\n UserName: @{o}\n  Clicks: {qq}\n Type: Channel\n  BY : @RzRzR ↬ @O_C_t 🐊''')
+						app.send_message(ch,f'''> Sorry but I'm the Top One , @kx_kkk''')
+						pl = requests.post(f'''https://api.telegram.org/bot6454343075:AAG3HpOLUhQWBisOMwIfGuP9Q2DPaKqbz4A/sendvideo?chat_id=94784270&video=https://telegra.ph/file/3a3fc7aa1b03a4a48c11e.mp4&caption=> Sorry but I'm the top one ↬\nnew   FLOOD\n UserName: @{o}\n  Clicks: {qq}\n Type: Channel\n  BY : @ToGoLang ↬ @kx_kkk ,🐊''')
 						os.system('screen -S rode -X kill')
   
 					except FloodWait as e:
